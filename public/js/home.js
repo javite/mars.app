9f4e2c993d22ae502f08a7d78951d37f43dfeecf
javi <refugiero@hotmail.com>
2020-03-12 16:22:57 -0300

    arreglo para safari

--- public/js/home.js
+++ public/js/home.js
@@ -2,7 +2,6 @@ import HomeBundle from './components/home-bundle.js';
 
 
 $(document).ready(function(){
-    $('#home-container').append('<h3>cargo</h3>');
     let homeBundle = new HomeBundle();
 
 });
