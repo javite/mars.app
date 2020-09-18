d6c70688c42775218439a9ef9f7dd832cce2061f
javite <refugiero@hotmail.com>
2020-09-18 09:48:56 -0300

    html

---
+++ public/workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "D:\\Proyectos\\mars.iot\\mars.app\\public",
+  "globPatterns": [
+    "**/*.{html,xml,css,ico,png,svg,php,js,json,txt,config}"
+  ],
+  "swDest": "D:/Proyectos/mars.iot/mars.app/public/sw.js",
+  "swSrc": "D:/Proyectos/mars.iot/mars.app/public/sw.js"
+};
