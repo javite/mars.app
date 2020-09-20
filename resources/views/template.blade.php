e0c43498f7cd9b4f428db8939d4fcc517e9565dd
javite <refugiero@hotmail.com>
2020-09-20 17:00:30 -0300

    elimine favicons

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -17,8 +17,8 @@
     <!-- iconos  -->
     <link rel="apple-touch-icon" href="images/icon.png">
     <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+    <!-- <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png"> -->
 
     <!-- <link rel="apple-touch-icon" sizes="152x152" href="images/icon.png">
     <link rel="apple-touch-icon" sizes="180x180" href="images/mars_180x180.png">
